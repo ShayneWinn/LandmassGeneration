4709c5110ee58abcec92ef1715d648ce96569658
ShayneWinn <shaynedwinn@gmail.com>
2019-12-06 14:12:21 -0500

    Created Unity file

---
+++ LandmassGeneration/Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRenderer;
+
+    public void DrawNoiseMap(float[,] _noiseMap)
+    {
+        int width = _noiseMap.GetLength(0);
+        int height = _noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        Color[] colorMap = new Color[width * height];
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < 0; x++)
+            {
+                colorMap[y * width + x] = _noiseMap[x, y];
+            }
+        }
+    }
+}
---
+++ LandmassGeneration/Assets/Scripts/Noise.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+
+    public static float[,] GenerateNoiseMap(int _mapWidth, int _mapHeight, float _scale)
+    {
+        float[,] noiseMap = new float[_mapWidth, _mapHeight];
+
+        if (_scale <= 0)
+        {
+            _scale = 0.001f;
+        }
+
+        for (int y = 0; y < _mapHeight; y++)
+        {
+            for (int x = 0; x < _mapWidth; x++)
+            {
+                float sampleX = x / _scale;
+                float sampleY = y / _scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
